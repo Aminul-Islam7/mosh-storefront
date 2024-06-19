462a5d4bdadf934e2e2313830f70d8f21eac00f2
Aminul Islam (Fardin) <aminulfardin7@gmail.com>
2024-06-19 21:49:06 +0600

    feat: Show products count and link to filter products by collection from collection list

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
 
 
@@ -33,4 +36,22 @@ class OrderAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            })
+        )
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
