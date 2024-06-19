ba6cb370bbd941da183b1badbd1ab810583355c9
Aminul Islam (Fardin) <aminulfardin7@gmail.com>
2024-06-19 18:57:54 +0600

    feat: Add computed inventory status column to product list

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,13 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        return 'Low' if product.inventory < 10 else 'Okay'
 
 
 @admin.register(models.Customer)
