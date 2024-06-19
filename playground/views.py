401ecf6c9a7dd14e30b6de51cce81a7cac85c963
Aminul Islam (Fardin) <aminulfardin7@gmail.com>
2024-06-19 17:49:08 +0600

    feat: Add custom header and title to Admin panel

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Q, F
+from store.models import Product, OrderItem, Order
 
 
 def say_hello(request):
-    x = 1
-    return render(request, 'hello.html', {'name': 'Fardin'})
-    y = 2
+    # queryset = Product.objects.values('id', 'title').filter(id=F('orderitem__product_id')).distinct().order_by('title')
+    # queryset = Product.objects.values('id', 'title').filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+
+    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # queryset = OrderItem.objects.filter(order_id__in=queryset)
+
+    # return render(request, 'hello.html', {'name': 'Fardin', 'products': [1, 2]})
+    # return render(request, 'hello.html', {'name': 'Fardin', 'products': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Fardin', 'orderitems': list(queryset)})
