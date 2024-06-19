183eb8592e31d38e41743f44561eb838dca2fd9d
Aminul Islam (Fardin) <aminulfardin7@gmail.com>
2024-06-19 17:54:06 +0600

    feat: Register and customize collection model for admin site

--- store/models.py
+++ store/models.py
@@ -33,6 +33,12 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
