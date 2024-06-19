00d8da0e74f2321de1439b5e2f1d18a812676ef8
Aminul Islam (Fardin) <aminulfardin7@gmail.com>
2024-06-19 22:02:13 +0600

    feat: Add search by name option to customer list

--- store/admin.py
+++ store/admin.py
@@ -26,13 +26,14 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
-    @admin.display(ordering='orders_count')
-    def orders_count(self, customer):
+    @admin.display(ordering='orders')
+    def orders(self, customer):
         url = (
             reverse('admin:store_order_changelist')
             + '?'
@@ -40,11 +41,11 @@ class CustomerAdmin(admin.ModelAdmin):
                 'customer__id': customer.id
             })
         )
-        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
-            orders_count=Count('order')
+            orders=Count('order')
         )
 
 
