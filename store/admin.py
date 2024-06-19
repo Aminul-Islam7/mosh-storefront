f4714e802a46846667ace05bd93e823d45bcba8a
Aminul Islam (Fardin) <aminulfardin7@gmail.com>
2024-06-19 18:11:56 +0600

    feat: Customize list page for customer

--- store/admin.py
+++ store/admin.py
@@ -9,4 +9,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
+
 admin.site.register(models.Collection)
